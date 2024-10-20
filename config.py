888d029f5adf868a67d21fc1b465fa170606b624
rahul0715 <ajaychouhan26367@gmail.com>
2024-10-20 11:18:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =27862677
+api_hash = "e343ce2c81b2b6c2c0d6bee58284e3bd"
+bot_token = "7063024370:AAFHuTYE6bOwjI5s3Nodv2mi8JGctq15nu0"
+auth_users = "5881684718"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "5881684718"
 owner_users = [int(num) for num in osowner_users.split(",")]
